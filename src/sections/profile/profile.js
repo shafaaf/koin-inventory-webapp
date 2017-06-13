54c9bd118e865947b5082fa1e506e6772c4cdd15
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-06-13 16:56:31 -0400

    working now

--- src/sections/profile/profile.js
+++ src/sections/profile/profile.js
@@ -4,12 +4,43 @@ import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 // Import styles
 import './styles.css';
 
+var spanStyles = {
+  fontSize:'30px',
+  cursor:'pointer'
+};
+
 export default class Profile extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
 			showNavBar: false
 		};
+	}
+
+	mySidenavStyles(){
+		if(this.state.showNavBar){
+			return {
+				width: "250px"
+			}
+		}
+		else{
+			return{
+				width: 0
+			}
+		}
+	}
+
+	mainStyles(){
+		if(this.state.showNavBar){
+			return {
+				marginLeft: "250px"
+			}
+		}
+		else{
+			return{
+				marginLeft: 0
+			}
+		}
 	}
 
 	facebookLogout(){
@@ -21,49 +52,33 @@ export default class Profile extends Component {
 	}
 
 	openNav(){
-		// var object = this.refs.mySidenav;
-		// object.style.width = "250px";
-    	
-		// var object2 = this.refs.main;
-		// object2.style.width = "250px";
 		console.log("At openNav");
 		this.setState({ showNavBar: true});
 	}
 
 	closeNav() {
-	 //    var object = this.refs.mySidenav;
-		// object.style.width = "0px";
-    	
-		// var object2 = this.refs.main;
-		// object2.style.width = "0px";
-
 		console.log("At closeNavbar");
 		this.setState({ showNavBar: false});
-	}
-
-	renderNavBarSection(){
-		if(this.state.showNavBar){
-			return (
-				<div id="mySidenav" className="sidenav" ref="mySidenav">
-					<a href="javascript:void(0)" className ="closebtn" onClick={this.closeNav.bind(this)}>&times;</a>
-					<a href="#">About</a>
-					<a href="#">Services</a>
-					<a href="#">Clients</a>
-					<a href="#">Contact</a>
-				</div>
-			)
-		}
 	}
 
 	render() {
 		return (
 			<div className = "container">
-				{this.renderNavBarSection()}
-				<div id="main" ref = "main">
-					<h1>Profile Page</h1>
-					<Button bsStyle="danger" onClick={this.openNav.bind(this)}>open</Button>
-					<Button id = "logoutButton" onClick={this.facebookLogout.bind(this)} bsSize="small" bsStyle="danger">Log outz</Button>
+				<div id="mySidenav" className="sidenav" style = {this.mySidenavStyles()}>
+					<a href="javascript:void(0)" className="closebtn" onClick={this.closeNav.bind(this)}>&times;</a>
+					<a href="#">About</a>
+					<a href="#">Services</a>
+					<a href="#">Clients</a>
+					<a href="#">Contact</a>
 				</div>
+				<div id="main" style = {this.mainStyles()}>
+					<h2>Sidenav Push Example</h2>
+					<p>Click on the element below to open the side navigation menu, and push this content to the right.</p>
+					<span style={spanStyles} onClick={this.openNav.bind(this)}>&#9776; open</span>
+				</div>
+				<h1>Profile Page</h1>
+				<Button bsStyle="danger" onClick={this.openNav.bind(this)}>open</Button>
+				<Button id = "logoutButton" onClick={this.facebookLogout.bind(this)} bsSize="small" bsStyle="danger">Log outz</Button>
 			</div>
 		);
 	}
