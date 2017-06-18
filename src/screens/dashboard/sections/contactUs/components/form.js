d85d3724ef43e44409e27299ace6c6ae494f6587
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-06-18 19:57:10 -0400

    back to regular bootstrap form. Will add in validator lib

--- src/screens/dashboard/sections/contactUs/components/form.js
+++ src/screens/dashboard/sections/contactUs/components/form.js
@@ -1,32 +1,26 @@
 import React,{Component} from 'react';
 
 
-// Bootstrap components
+// // Bootstrap components
 import { FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 
+// Form Validation components
+import { ButtonInput } from 'react-bootstrap';
 
 export default class Form extends Component {
 	constructor(props) {
 	    console.log("On constructor");
 	    super(props);
 	    this.state = {
-        	value: '',
-        	error: ''
-      };
+			value: ''
+		};
 	}
 
-	getNameValidationState() {	//Do all the validation checks here
-		var name = this.state.value;
-		if(this.state.value == "")
-		{
-			return 'error';
-		}
-		if (name.indexOf(' ') >= 0){	//checking for spaces in first name
-			return 'error';
-		}
-		else{
-			return 'success';
-		}
+	getValidationState(){
+		const length = this.state.value.length;
+		if (length > 10) return 'success';
+		else if (length > 5) return 'warning';
+		else if (length > 0) return 'error';
 	}
 
 	handleChange(e) {
@@ -36,11 +30,11 @@ export default class Form extends Component {
 	render() {
 		return (
 			<form>
-				<FormGroup controlId="formBasicText" validationState = {this.getNameValidationState()}>
-					<ControlLabel>First Name</ControlLabel>
-					<FormControl type="text" placeholder="Enter text" onChange={this.handleChange.bind(this)}/>
-					<FormControl.Feedback />
-					<HelpBlock>{this.state.error}</HelpBlock>
+				<FormGroup controlId="formBasicText" validationState={this.getValidationState()}>
+					<ControlLabel>Working example with validation</ControlLabel>
+					<FormControl type="text" value={this.state.value} placeholder="Enter text" onChange={this.handleChange.bind(this)}/>
+					<FormControl.Feedback/>
+					<HelpBlock>Validation is based on string length.</HelpBlock>
 				</FormGroup>
 			</form>
 		);
