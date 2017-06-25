7a6e84e708e98204a47f3bcdc58091b08a2e3352
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-06-24 20:19:50 -0400

    sidenav in add inventory sort of works but very hacky

--- src/screens/dashboard/sections/inventory/listInventory/listInventory.js
+++ src/screens/dashboard/sections/inventory/listInventory/listInventory.js
@@ -1,14 +1,24 @@
 import React,{Component} from 'react';
+import { Sidebar, SidebarItem } from 'react-responsive-sidebar';
 
 // Components
 import InventoryGallery from './components/inventoryGallery.js';
+
+const items = [
+  <SidebarItem>Dashboard</SidebarItem>,
+  <SidebarItem>Profile</SidebarItem>,
+  <SidebarItem>Settings</SidebarItem>,
+];
+
 
 export default class ListInventory extends Component {
   render() {
     return (
     	<div>
-			<h2>Your Inventory!</h2>
-			<InventoryGallery/>
+    		<Sidebar style = {{paddingTop:"100px"}} background = {"#000000"} color = {"#818181"} width = {150} content={items}>
+				<h2 style = {{paddingTop:"40px"}}>Your Inventory!</h2>
+    			<InventoryGallery/>
+  			</Sidebar>
 		</div>
     );
   }
