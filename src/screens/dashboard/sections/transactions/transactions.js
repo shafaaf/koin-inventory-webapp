de068f00b14265a30c59cdbea56d9a1a5b3c4e2d
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-07-01 20:53:59 -0400

    have converted date. Need to expand table

--- src/screens/dashboard/sections/transactions/transactions.js
+++ src/screens/dashboard/sections/transactions/transactions.js
@@ -5,6 +5,7 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 require('react-bootstrap-table/dist/react-bootstrap-table-all.min.css');
 
 
+
 // products will be presented by react-bootstrap-table
 var products = [{
       id: 1,
@@ -21,25 +22,36 @@ function priceFormatter(cell, row){
   return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
 }
 
+function timeFormatter(cell, row){
+	//console.log("cell is: ", cell);
+	//console.log("row is: ", row);
+	//return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
+	var moment = require('moment');
+	var a = moment(cell);
+
+	return <p>{a.format("MMM Do YYYY")}</p>;
+}
+
+
 export default class Transactions extends Component {
-  constructor(props){
+  constructor(props) {
   	super(props);
 	this.state = {
 		loading: true,
 		transactionsList: [],
 		hasNextPage: null,
-		currentTransactionPage: null,
-		open: true
+		tableData: [],
+		currentTransactionPage: null
 	};
   }
 
-  componentWillMount(){
+  componentWillMount() {
   	console.log("componentWillMount here.");
   	// Todo: heard not good
   }
 
   // Initial fetch of merchant's transactions
-  componentDidMount(){
+  componentDidMount() {
   	console.log("componentDidMount here. Going to fetch transactions");
   	// Requesting transaction info from Koin server
 	// Todo: Hard coded right now using Zen's session token
@@ -64,30 +76,48 @@ export default class Transactions extends Component {
 			// Examine the text in the response from Koin server
 			response.json().then(function(data) {  
 				console.log("Transaction data from server is: ", data);
+				
 				console.log("setting state for transactions now.");
 				// Todo: get warning of setting state when component not mounted
 				// Happens when click away to another section and state for old component
 				// is being set
+				
+				// Setup table data
+				var tableData = thisContext.state.tableData;
+				console.log("tableData is: ", tableData);
+				var dataListLength = data["transactions"].length;
+				var i = 0;
+				for(i = 0; i < dataListLength; i++){
+					var myEntry = {};
+					myEntry["dateTime"] = data["transactions"][i]["created_at"];
+					myEntry["amount"] = data["transactions"][i]["amount"];
+					myEntry["storeName"] = data["transactions"][i]["merchant"]["store_name"];
+					myEntry["state"] = data["transactions"][i]["state"];
+					tableData.push(myEntry);
+				}
+
 				thisContext.setState({
 					loading: false,
 					transactionsList: data["transactions"],
-					currentTransactionPage: 1,
-					hasNextPage: data["has_next_page"]
+					hasNextPage: data["has_next_page"],
+					tableData: tableData,
+					currentTransactionPage: 1
 				 });
 			});
 		}
 	);
 	console.log("After promise section in componentDidMount transactions fetch.");
   }
-  
 
-  fetchDifferentIndexTransactions(direction){
+
+  // Todo: Figure out how to other fetch data here later on.
+  fetchDifferentIndexTransactions(direction) {
   	console.log("fetchDifferentIndexTransactions: direction is: ", direction);
   	console.log("current index is: ", this.state.currentTransactionPage);
   	var currentIndex = this.state.currentTransactionPage;
   	
   	// Todo: perform checks to see if possible or not to get other index
-  	if(direction == "prev"){
+  	if(direction == "prev") {
   		currentIndex = currentIndex - 1;
   		if(currentIndex < 1){
   			console.log("currentIndex<1 so returning");
@@ -129,6 +159,8 @@ export default class Transactions extends Component {
 				// Todo: get warning of setting state when component not mounted
 				// Happens when click away to another section and state for old component
 				// is being set
+				//
+
 				thisContext.setState({
 					loading: false,
 					transactions: data,
@@ -145,10 +177,6 @@ export default class Transactions extends Component {
 		loading: true
 	});
   }
-
-  	formatTable(){
-  		
-  	}
 
   render() {
   	console.log("Rendering transactions component.");
@@ -171,6 +199,13 @@ export default class Transactions extends Component {
 				</BootstrapTable>
 
 				<h3>Final table</h3>
+				<BootstrapTable data={this.state.tableData} striped={true} hover={true}>
+					<TableHeaderColumn dataField="dateTime" dataFormat={timeFormatter} isKey={true} dataAlign="center" dataSort={true}>DateTime</TableHeaderColumn>
+					<TableHeaderColumn dataField="amount" dataSort={true}>Amount</TableHeaderColumn>
+					<TableHeaderColumn dataField="storeName" dataSort={true}>Store Name</TableHeaderColumn>
+					<TableHeaderColumn dataField="state" dataSort={true}>State</TableHeaderColumn>
+				</BootstrapTable>
+				
 				
 				{/* Not working for now*/}
 				<button onClick = {this.fetchDifferentIndexTransactions.bind(this,"prev")}>Prev</button> 
