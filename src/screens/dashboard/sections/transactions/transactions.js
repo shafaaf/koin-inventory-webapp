93290d06e7718c4702d6cb479dca9a7654e98f90
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-07-12 17:55:44 -0400

    the expandable problem cause of not including options - WEIRD

--- src/screens/dashboard/sections/transactions/transactions.js
+++ src/screens/dashboard/sections/transactions/transactions.js
@@ -1,8 +1,15 @@
 import React,{Component} from 'react';
 import Moment from 'react-moment';
 
+import ExpandedRow from './components/expandedRow';
+import MyDatePicker from './components/myDatePicker';
+
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import {Grid, Row, Col, Button, Pager} from 'react-bootstrap';
+
+
+require('react-bootstrap-table/dist/react-bootstrap-table-all.min.css');
+
 
 // Functions to format values in table
 function priceFormatter(cell, row){
@@ -15,67 +22,16 @@ function timeFormatter(cell, row){
   return <p>{a.format("MMM Do YYYY")}</p>;
 }
 
-const products = [];
-
-function addProducts(quantity) {
-  const startId = products.length;
-  for (let i = 0; i < quantity; i++) {
-    const id = startId + i;
-    if (i < 3) {
-      products.push({
-        id: id,
-        name: 'Item name ' + id,
-        price: 2100 + i,
-        expand: [ {
-          fieldA: 'test1',
-          fieldB: (i + 1) * 99,
-          fieldC: (i + 1) * Math.random() * 100,
-          fieldD: '123eedd' + i
-        }, {
-          fieldA: 'test2',
-          fieldB: i * 99,
-          fieldC: i * Math.random() * 100,
-          fieldD: '123eedd' + i
-        } ]
-      });
-    } else {
-      products.push({
-        id: id,
-        name: 'Item name ' + id,
-        price: 2100 + i
-      });
-    }
-  }
-}
-addProducts(5);
-
-
-class BSTable extends React.Component {
-  render() {
-    if (this.props.data) {
-      return (
-        <BootstrapTable data={ this.props.data }>
-          <TableHeaderColumn dataField='fieldA' isKey={ true }>Field A</TableHeaderColumn>
-          <TableHeaderColumn dataField='fieldB'>Field B</TableHeaderColumn>
-          <TableHeaderColumn dataField='fieldC'>Field C</TableHeaderColumn>
-          <TableHeaderColumn dataField='fieldD'>Field D</TableHeaderColumn>
-        </BootstrapTable>);
-    } else {
-      return (<p>?</p>);
-    }
-  }
-}
-
 export default class Transactions extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loading: true,
-      transactionsList: [],
-      tableData: [],
-      hasNextPage: null,
-      currentTransactionPage: null
-    };
+  this.state = {
+    loading: true,
+    transactionsList: [],
+    tableData: [],
+    hasNextPage: null,
+    currentTransactionPage: null
+  };
   }
 
   componentWillMount() {
@@ -88,59 +44,59 @@ export default class Transactions extends Component {
   componentDidMount() {
     console.log("componentDidMount here. Going to fetch transactions");
     // Requesting transaction info from Koin server
-    // Todo: Hard coded right now using Zen's session token
-    var data = JSON.stringify({});
-    var url = 'http://custom-env-1.2tfxydg93p.us-west-2.elasticbeanstalk.com/api/v1/transactions/merchant/list';
-    var request = new Request(url, {
-      method: 'POST',
-      body: data,
-      mode: 'cors',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 6849c19749955194e6f51c5a69ac28b2aac08ade'  // Zen's token hardcoded in
-      })
-    });
-    var thisContext = this; // To keep track of this context within promise callback
-    fetch(request).then(
-      function(response) {
-        if (response.status !== 200) {   
-          console.log('Looks like there was a problem. Status Code: ' +  response.status);  
-          return;  
-        }
-        // Examine the text in the response from Koin server
-        response.json().then(function(data) {  
-          console.log("Transaction data from server is: ", data);
-            
-          // Setup table data
-          console.log("Setting table for transactions now.");
-          var tableData = thisContext.state.tableData;
-          var dataListLength = data["transactions"].length;
-          var i = 0;
-          for(i = 0; i < dataListLength; i++){
-            var myEntry = {};
-            myEntry["dateTime"] = data["transactions"][i]["created_at"];
-            myEntry["amount"] = data["transactions"][i]["amount"];
-            myEntry["state"] = data["transactions"][i]["state"];
-
-            // To show in expandable row
-            myEntry["storeName"] = data["transactions"][i]["merchant"]["store_name"];
-            myEntry["storeLocation"] = data["transactions"][i]["merchant"]["store_location"];
-            myEntry["storeType"] = data["transactions"][i]["merchant"]["store_type"];
-            tableData.push(myEntry);
-          }
-
-          console.log("Setting state for transactions now.");
-          thisContext.setState({
-            loading: false,
-            transactionsList: data["transactions"],
-            hasNextPage: data["has_next_page"],
-            tableData: tableData,
-            currentTransactionPage: 1
-          });
-        });
+  // Todo: Hard coded right now using Zen's session token
+  var data = JSON.stringify({});
+  var url = 'http://custom-env-1.2tfxydg93p.us-west-2.elasticbeanstalk.com/api/v1/transactions/merchant/list';
+  var request = new Request(url, {
+    method: 'POST',
+    body: data,
+    mode: 'cors',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 6849c19749955194e6f51c5a69ac28b2aac08ade'  // Zen's token hardcoded in
+    })
+  });
+  var thisContext = this; // To keep track of this context within promise callback
+  fetch(request).then(
+    function(response) {
+      if (response.status !== 200) {   
+        console.log('Looks like there was a problem. Status Code: ' +  response.status);  
+        return;  
       }
-    );
-    console.log("After promise section in componentDidMount transactions fetch.");
+      // Examine the text in the response from Koin server
+      response.json().then(function(data) {  
+        console.log("Transaction data from server is: ", data);
+                
+        // Setup table data
+        console.log("Setting table for transactions now.");
+        var tableData = thisContext.state.tableData;
+        var dataListLength = data["transactions"].length;
+        var i = 0;
+        for(i = 0; i < dataListLength; i++){
+          var myEntry = {};
+          myEntry["dateTime"] = data["transactions"][i]["created_at"];
+          myEntry["amount"] = data["transactions"][i]["amount"];
+          myEntry["state"] = data["transactions"][i]["state"];
+          
+          // To show in expandable row
+          myEntry["storeName"] = data["transactions"][i]["merchant"]["store_name"];
+          myEntry["storeLocation"] = data["transactions"][i]["merchant"]["store_location"];
+          myEntry["storeType"] = data["transactions"][i]["merchant"]["store_type"];
+          tableData.push(myEntry);
+        }
+
+        console.log("Setting state for transactions now.");
+        thisContext.setState({
+          loading: false,
+          transactionsList: data["transactions"],
+          hasNextPage: data["has_next_page"],
+          tableData: tableData,
+          currentTransactionPage: 1
+         });
+      });
+    }
+  );
+  console.log("After promise section in componentDidMount transactions fetch.");
   }
 
   componentWillUnmount(){
@@ -232,36 +188,82 @@ export default class Transactions extends Component {
   // });
   }
 
-    isExpandableRow(row) {
-      if (row.id < 3) return true;
-      else return false;
+  isExpandableRow(row) {
+    return true;
   }
 
   expandComponent(row) {
     return (
-      <BSTable data={ row.expand } />
+      <ExpandedRow storeName={ row.storeName } storeLocation={ row.storeLocation } storeType = {row.storeType} />
     );
   }
 
+  renderCurrentDate(){
+    // Todo: Do for Dhaka time
+    var fullDate = new Date();
+    console.log("fullDate is: ", fullDate);   
+    
+    var indexToMonth = new Array();
+    indexToMonth[0] = "January";
+    indexToMonth[1] = "February";
+    indexToMonth[2] = "March";
+    indexToMonth[3] = "April";
+    indexToMonth[4] = "May";
+    indexToMonth[5] = "June";
+    indexToMonth[6] = "July";
+    indexToMonth[7] = "August";
+    indexToMonth[8] = "September";
+    indexToMonth[9] = "October";
+    indexToMonth[10] = "November";
+    indexToMonth[11] = "December";
+
+    var month = indexToMonth[fullDate.getMonth()];
+    var year = fullDate.getFullYear();
+    console.log("year is: ", year);
+    console.log("month is: ", month);
+    return month + " " + year;
+  }
 
   render() {
+    console.log("Rendering transactions component.");
+  if(this.state.loading){ // Show loading screen when getting data
+    return <h2>Loading your transactions ..</h2>;
+  }
+  else{ // Show transactions data
     const options = {
-      expandRowBgColor: 'rgb(242, 255, 163)'
+        expandRowBgColor: 'rgb(242, 255, 163)'
     };
     return (
-      <BootstrapTable data={ products }
-        options={ options }
+        <div>
+        <h2>Your Transactions!</h2>
+        <p>Fell free to check out your transactions!</p>
+        <MyDatePicker/>
+
+        <Pager>
+            <Pager.Item previous onClick = {this.fetchDifferentIndexTransactions.bind(this,"prev")}>&larr; Previous Page</Pager.Item>
+            <Pager.Item next     onClick = {this.fetchDifferentIndexTransactions.bind(this,"next")}>Next Page &rarr;</Pager.Item>
+          </Pager>
+
+        <h3 style = {{textAlign: "center"}}>Transactions for {this.renderCurrentDate()}</h3>
+        
+        <BootstrapTable data={this.state.tableData} hover={true} options={ options }
         expandableRow={ this.isExpandableRow }
-        expandComponent={ this.expandComponent }
-        expandColumnOptions={ { expandColumnVisible: true } }
-        search>
-        <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
-        <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-        <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
-      </BootstrapTable>
-    );
+        expandComponent={ this.expandComponent}
+        expandColumnOptions={{expandColumnVisible: true}}>
+          <TableHeaderColumn dataField="dateTime" dataFormat={timeFormatter} isKey={true} dataAlign="center" dataSort={true}>DateTime</TableHeaderColumn>
+          <TableHeaderColumn dataField="amount" dataFormat={priceFormatter} dataSort={true}>Amount</TableHeaderColumn>
+          <TableHeaderColumn dataField="state" dataSort={true}>State</TableHeaderColumn>
+        </BootstrapTable>
+
+        <Pager>
+            <Pager.Item previous onClick = {this.fetchDifferentIndexTransactions.bind(this,"prev")}>&larr; Previous Page</Pager.Item>
+            <Pager.Item next     onClick = {this.fetchDifferentIndexTransactions.bind(this,"next")}>Next Page &rarr;</Pager.Item>
+          </Pager>      
+      
+      </div>
+      );
   }
+  }
+
 }
-
-
 
