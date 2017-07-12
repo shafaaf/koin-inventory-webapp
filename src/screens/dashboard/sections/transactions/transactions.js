5b2d90e719669ded1f113ffac88053dbdcc4e83d
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-07-12 18:55:05 -0400

    getting more transactions button cnoditional

--- src/screens/dashboard/sections/transactions/transactions.js
+++ src/screens/dashboard/sections/transactions/transactions.js
@@ -7,9 +7,7 @@ import MyDatePicker from './components/myDatePicker';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import {Grid, Row, Col, Button, Pager} from 'react-bootstrap';
 
-
 require('react-bootstrap-table/dist/react-bootstrap-table-all.min.css');
-
 
 // Functions to format values in table
 function priceFormatter(cell, row){
@@ -44,66 +42,68 @@ export default class Transactions extends Component {
   componentDidMount() {
     console.log("componentDidMount here. Going to fetch transactions");
     // Requesting transaction info from Koin server
-  // Todo: Hard coded right now using Zen's session token
-  var data = JSON.stringify({
-    "query_parameters":
-    {
-      "updates_after" : "1498881600000",
-      "updates_before" : "1499832000000",
-      "order" : "DESCENDING"
-    }
-  });
-  var url = 'http://custom-env-1.2tfxydg93p.us-west-2.elasticbeanstalk.com/api/v1/transactions/merchant/list';
-  var request = new Request(url, {
-    method: 'POST',
-    body: data,
-    mode: 'cors',
-    headers: new Headers({
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer 6849c19749955194e6f51c5a69ac28b2aac08ade'  // Zen's token hardcoded in
-    })
-  });
-  var thisContext = this; // To keep track of this context within promise callback
-  fetch(request).then(
-    function(response) {
-      if (response.status !== 200) {   
-        console.log('Looks like there was a problem. Status Code: ' +  response.status);  
-        return;  
+    // Todo: Hard coded right now using Zen's session token
+    var data = JSON.stringify({
+      "query_parameters":
+      {
+        "updates_after" : "1498881600000",
+        "updates_before" : "1499832000000",
+        "order" : "DESCENDING"
       }
-      // Examine the text in the response from Koin server
-      response.json().then(function(data) {  
-        console.log("Transaction data from server is: ", data);
-                
-        // Setup table data
-        console.log("Setting table for transactions now.");
-        var tableData = thisContext.state.tableData;
-        var dataListLength = data["transactions"].length;
-        var i = 0;
-        for(i = 0; i < dataListLength; i++){
-          var myEntry = {};
-          myEntry["dateTime"] = data["transactions"][i]["created_at"];
-          myEntry["amount"] = data["transactions"][i]["amount"];
-          myEntry["state"] = data["transactions"][i]["state"];
-          
-          // To show in expandable row
-          myEntry["storeName"] = data["transactions"][i]["merchant"]["store_name"];
-          myEntry["storeLocation"] = data["transactions"][i]["merchant"]["store_location"];
-          myEntry["storeType"] = data["transactions"][i]["merchant"]["store_type"];
-          tableData.push(myEntry);
+    });
+    var url = 'http://custom-env-1.2tfxydg93p.us-west-2.elasticbeanstalk.com/api/v1/transactions/merchant/list';
+    var request = new Request(url, {
+      method: 'POST',
+      body: data,
+      mode: 'cors',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 6849c19749955194e6f51c5a69ac28b2aac08ade'  // Zen's token hardcoded in
+      })
+    });
+    var thisContext = this; // To keep track of this context within promise callback
+    fetch(request).then(
+      function(response) {
+        if (response.status !== 200) {   
+          console.log('Looks like there was a problem. Status Code: ' +  response.status);  
+          return;  
         }
+        // Examine the text in the response from Koin server
+        response.json().then(function(data) {  
+          console.log("componentDidMount: Transaction data from server is: ", data);
+                  
+          // Setup table data
+          console.log("Setting table for transactions now.");
+          var tableData = thisContext.state.tableData;
+          var dataListLength = data["transactions"].length;
+          var i = 0;
+          for(i = 0; i < dataListLength; i++){
+            var myEntry = {};
+            myEntry["dateTime"] = data["transactions"][i]["created_at"];
+            myEntry["amount"] = data["transactions"][i]["amount"];
+            myEntry["state"] = data["transactions"][i]["state"];
+            
+            // To show in expandable row
+            myEntry["storeName"] = data["transactions"][i]["merchant"]["store_name"];
+            myEntry["storeLocation"] = data["transactions"][i]["merchant"]["store_location"];
+            myEntry["storeType"] = data["transactions"][i]["merchant"]["store_type"];
+            tableData.push(myEntry);
+          }
 
-        console.log("Setting state for transactions now.");
-        thisContext.setState({
-          loading: false,
-          transactionsList: data["transactions"],
-          hasNextPage: data["has_next_page"],
-          tableData: tableData,
-          currentTransactionPage: 1
-         });
-      });
-    }
-  );
-  console.log("After promise section in componentDidMount transactions fetch.");
+          console.log("Setting state for transactions now.");
+          console.log("has_next_page is: ", data["has_next_page"]);
+          
+          thisContext.setState({
+            loading: false,
+            transactionsList: data["transactions"],
+            hasNextPage: data["has_next_page"],
+            tableData: tableData,
+            currentTransactionPage: 1
+           });
+        });
+      }
+    );
+    console.log("After promise section in componentDidMount transactions fetch.");
   }
 
   componentWillUnmount(){
@@ -111,29 +111,16 @@ export default class Transactions extends Component {
   }
 
   // Todo: Figure out how to other fetch data here later on.
-  fetchDifferentIndexTransactions(direction) {
-    console.log("fetchDifferentIndexTransactions: direction is: ", direction);
+  fetchDifferentIndexTransactions() {
     console.log("current index is: ", this.state.currentTransactionPage);
     var currentIndex = this.state.currentTransactionPage;
     
-    // Todo: perform checks to see if possible or not to get other index
-    if(direction == "prev") {
-      currentIndex = currentIndex - 1;
-      if(currentIndex < 1){
-        console.log("currentIndex<1 so returning");
-        return;
-      }
-    }
-    else if(direction == "next"){
-      currentIndex = currentIndex + 1;
-      if(!this.state.hasNextPage){
+    // Todo: perform checks to see if possible or not to get next pages
+    if(!this.state.hasNextPage){
         console.log("Theres no more pages.");
         return;
       }
-    }
-    else{
-      console.log("Some weird error!");
-    }
+    currentIndex = currentIndex + 1;
     var data = JSON.stringify({
       "query_parameters":
       {
@@ -161,12 +148,12 @@ export default class Transactions extends Component {
       }
       // Examine the text in the response from Koin server
       response.json().then(function(data) {  
-        console.log("transaction data from server is: ", data);
+        console.log("fetchDifferentIndexTransactions: transaction data from server is: ", data);
         
         // Setup table data
         console.log("Setting table for prev/next transactions now.");
         var tableData = thisContext.state.tableData;
-        tableData.splice(0,tableData.length);
+        //tableData.splice(0,tableData.length);
         var dataListLength = data["transactions"].length;
         var i = 0;
         for(i = 0; i < dataListLength; i++){
@@ -237,45 +224,51 @@ export default class Transactions extends Component {
     return month + " " + year;
   }
 
+  renderButtonOrFinishMessage(){
+    console.log("At renderButtonOrFinishMessage");
+    if(this.state.hasNextPage){
+      console.log("hasNextPage");
+      return(
+        <Button onClick = {this.fetchDifferentIndexTransactions.bind(this)}>Generate More ...</Button>
+      );
+    }
+    else
+    {
+      console.log("no hasNextPage");
+      return <p>No more left</p>;
+    }
+  }
+
   render() {
     console.log("Rendering transactions component.");
-  if(this.state.loading){ // Show loading screen when getting data
-    return <h2>Loading your transactions ..</h2>;
-  }
-  else{ // Show transactions data
-    const options = {
+    if(this.state.loading){ // Show loading screen when getting data
+      return <h2>Loading your transactions ..</h2>;
+    }
+    else{ // Show transactions data
+      const options = {
         expandRowBgColor: 'rgb(242, 255, 163)'
-    };
-    return (
+      };
+      return (
         <div>
-        <h2>Your Transactions!</h2>
-        <p>Fell free to check out your transactions!</p>
-        <MyDatePicker/>
+          <h2>Your Transactions!</h2>
+          <p>Fell free to check out your transactions!</p>
+          <MyDatePicker/>
 
-        <Pager>
-          <Pager.Item previous onClick = {this.fetchDifferentIndexTransactions.bind(this,"prev")}>&larr; Previous Page</Pager.Item>
-          <Pager.Item next     onClick = {this.fetchDifferentIndexTransactions.bind(this,"next")}>Next Page &rarr;</Pager.Item>
-        </Pager>
-
-        <h3 style = {{textAlign: "center"}}>Transactions for {this.renderCurrentDate()}</h3>
-        
-        <BootstrapTable data={this.state.tableData} hover={true} options={ options }
-          expandableRow={ this.isExpandableRow }
-          expandComponent={ this.expandComponent}
-          expandColumnOptions={{expandColumnVisible: true}}>
-            <TableHeaderColumn dataField="dateTime" dataFormat={timeFormatter} isKey={true} dataAlign="center" dataSort={true}>DateTime</TableHeaderColumn>
-            <TableHeaderColumn dataField="amount" dataFormat={priceFormatter} dataSort={true}>Amount</TableHeaderColumn>
-            <TableHeaderColumn dataField="state" dataSort={true}>State</TableHeaderColumn>
-        </BootstrapTable>
-
-        <Pager>
-            <Pager.Item previous onClick = {this.fetchDifferentIndexTransactions.bind(this,"prev")}>&larr; Previous Page</Pager.Item>
-            <Pager.Item next     onClick = {this.fetchDifferentIndexTransactions.bind(this,"next")}>Next Page &rarr;</Pager.Item>
-          </Pager>      
-      
-      </div>
+          <h3 style = {{textAlign: "center"}}>Transactions for {this.renderCurrentDate()}</h3>
+          
+          <BootstrapTable data={this.state.tableData} hover={true} options={ options }
+            expandableRow={ this.isExpandableRow }
+            expandComponent={ this.expandComponent}
+            expandColumnOptions={{expandColumnVisible: true}}>
+              <TableHeaderColumn dataField="dateTime" dataFormat={timeFormatter} isKey={true} dataAlign="center" dataSort={true}>DateTime</TableHeaderColumn>
+              <TableHeaderColumn dataField="amount" dataFormat={priceFormatter} dataSort={true}>Amount</TableHeaderColumn>
+              <TableHeaderColumn dataField="state" dataSort={true}>State</TableHeaderColumn>
+          </BootstrapTable>
+          {this.renderButtonOrFinishMessage()}
+          <p>Chcker</p>
+        </div>
       );
-  }
+    }
   }
 
 }
